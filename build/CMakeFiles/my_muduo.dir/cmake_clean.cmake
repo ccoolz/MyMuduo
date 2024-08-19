ebd4a20ddb8e8fb46c8acc5271c68ebf395bc285
ccoolz <3131974121@qq.com>
2024-08-20 03:24:15 +0800

    integrate all files and reconstruct the structure

---
+++ build/CMakeFiles/my_muduo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_muduo.dir/src/InetAddress.o"
+  "CMakeFiles/my_muduo.dir/src/Logger.o"
+  "CMakeFiles/my_muduo.dir/src/Timestamp.o"
+  "../lib/libmy_muduo.pdb"
+  "../lib/libmy_muduo.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_muduo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ inc/TcpServer.h
@@ -0,0 +1,15 @@
+#ifndef _TCP_SERVER_H_
+#define _TCP_SERVER_H_
+
+namespace zr
+{
+
+class TcpServer
+{
+
+};
+
+
+} // namespace zr
+
+#endif // _TCP_SERVER_H_
